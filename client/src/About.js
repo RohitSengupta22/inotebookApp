0ad344795808b1c173d1c8213b50d0be6077fc73
RohitSengupta22 <chints.rsg@gmail.com>
2023-08-17 20:41:51 +0530

    react project setup

---
+++ client/src/About.js
@@ -0,0 +1,13 @@
+import NavbarComp from "./Navbar";
+
+const About = () => {
+    return (
+        <>
+            <NavbarComp />
+            <h1>This is about us page of iNotebook</h1>
+        </>
+
+    )
+}
+
+export default About;
---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+import NavbarComp from "./Navbar";
+
+const Home = () =>{
+    return(
+        <>
+        <NavbarComp />
+        <h1>Welcome to iNotebook Homepage</h1>
+        </>
+        
+    )
+}
+
+export default Home;
